4da55c77cb1970988351896af0110bf38d551e8d
mdubelbeis <mason.dubelbeis@gmail.com>
2022-05-27 21:42:36 -0400

    added links to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,6 +12,20 @@ const Navbar = ({ title }) => {
             {title}
           </Link>
         </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="btn btn-ghost btn-sm rounded-btn"
+            >
+              About
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
